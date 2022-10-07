0f4323087618d08b9914f39e1d80c0371813d9bc
gkum4 <gustavokumasawa@gmail.com>
2022-10-07 09:54:43 -0300

    Initial Commit

---
+++ NavStackExample/NavStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavStackExampleApp.swift
+//  NavStackExample
+//
+//  Created by Gustavo Kumasawa on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct NavStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
